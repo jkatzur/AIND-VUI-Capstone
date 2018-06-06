e27700c0f55ca3f9755799dc91d46ebaa91df372
Ubuntu <ubuntu@ip-172-31-54-61.ec2.internal>
2018-06-06 05:31:22 +0000

    Completed model 3

--- sample_models.py
+++ sample_models.py
@@ -60,7 +60,7 @@ def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
     # Add batch normalization
     bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
     # Add recurrent layer
-    rnn = SimpleRNN(units, activation='relu',
+    rnn = GRU(units, activation='relu',
         return_sequences=True, implementation=2, name='rnn')(bn_cnn)
     bn_rnn = BatchNormalization(name='bn_rnn')(rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
@@ -137,18 +137,44 @@ def bidirectional_rnn_model(input_dim, units, output_dim=29):
     print(model.summary())
     return model
 
-def final_model():
+def final_model(input_dim, units, recur_layers, output_dim=29):
     """ Build a deep network for speech 
     """
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Specify the layers in your network
-    ...
+    recur_layer_data = input_data
+    # First, I chose to enable deep rnns because the deep rnn approach performed better than the standard rnn
+    for recur_layer in range(0,recur_layers):
+        recur_layer_name = 'recur_layer_' + str(recur_layer)
+        """
+            I chose to make my deep rnns using BiDirectional rnns becase the basic BiDirectional RNN performed
+            exceedingly well compared to the simple_rnn model, even without any batch normalization - which was
+            crucial in getting the rnn_model to perform well.
+        
+            I also tweaked my deep rnn from above to add dropout_W and dropout_U to the rnns. Dropout_W drops
+            some input data at the start of the model. My intuition is to avoid overfitting between layers
+            so I included this at .1 = 10% of input data dropped each run. 
+            
+            I also include dropout_U. Dropout_U drops values out between recurrent connections. This helps us
+            avoid overfitting within a given epoch for each RNN. 
+            
+            Given RNNs penchant for overfitting I wanted to experiment with both options. The idea to include
+            dropouts came from the documentation and the referred paper here: https://arxiv.org/pdf/1512.05287.pdf
+        
+        """
+        deep_bi_rnn = Bidirectional(GRU(units, activation='relu', return_sequences=True, 
+                 implementation=2, name=recur_layer_name, dropout_W=0.1, dropout_U=.2), merge_mode='concat')(recur_layer_data)
+        bnn_layer_name = 'bnn_layer_' + str(recur_layer)
+        # Batch normalization is included because we saw how effective this would be
+        recur_layer_data = BatchNormalization(name=bnn_layer_name)(deep_bi_rnn)
+    
+    time_dense = TimeDistributed(Dense(output_dim))(recur_layer_data)
     # TODO: Add softmax activation layer
-    y_pred = ...
+    y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
     model = Model(inputs=input_data, outputs=y_pred)
     # TODO: Specify model.output_length
-    model.output_length = ...
+    model.output_length = lambda x: x
     print(model.summary())
     return model
